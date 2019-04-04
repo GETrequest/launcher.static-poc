074173c577fa592929eae4c07226e7b10aed14a7
Philipp Wagner <wagner@getrequest.de>
2019-04-04 21:48:40 +0200

    add toggle menu function (#2)

--- 01/src/launcher.js
+++ 01/src/launcher.js
@@ -1,9 +1,21 @@
 const launcher = {
-    draw: () => {
+    toggleMenu: () => {
         const menu = document.getElementById('menu');
-        console.log('open launcher menu');
+        const open = document.getElementById('open');
         
-        
+        menu.style.visibility = menu.style.visibility === 'visible' ? 'hidden' : 'visible';
+        open.style.visibility = open.style.visibility === 'visible' ? 'hidden' : 'visible';
+    },
+
+    init: () => {
+        const menu = document.getElementById('menu');
+        const open= document.getElementById('open');
+        const container = document.getElementById('container');
+
+        open.style.visibility = 'visible';
+        menu.style.visibility = 'hidden';
+        container.style.visibility = 'hidden';
     }
 }
 
+launcher.init();
